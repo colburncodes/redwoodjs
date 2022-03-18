172766c5d286e38bb5957e1751c9889c4b16bb4a
colburncodes <colburnsanders@gmail.com>
2022-03-18 17:55:51 -0500

    add new pages and cleaned up navigation

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,16 @@
+// web/src/pages/HomePage/HomePage.js
+
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags title="Home" description="Home page" />
+      <main>
+        <p>Welcome To Home Page!</p>
+      </main>
+    </>
+  )
+}
+
+export default HomePage
